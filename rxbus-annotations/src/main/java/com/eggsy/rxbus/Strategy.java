ccb555c90b8ff2e08ac1b56028cb20c6b0be9e53
eggsywelsh <eggsywelsh@163.com>
2017-02-02 20:56:08 +0800

    modify back pressure strategy

---
+++ rxbus-annotations/src/main/java/com/eggsy/rxbus/Strategy.java
@@ -0,0 +1,37 @@
+package com.eggsy.rxbus;
+
+/**
+ * Created by eggsy on 17-2-2.
+ */
+
+public enum Strategy {
+
+    /**
+     * OnNext events are written without any buffering or dropping.
+     * Downstream has to deal with any overflow.
+     * <p>Useful when one applies one of the custom-parameter onBackpressureXXX operators.
+     */
+    MISSING,
+    /**
+     * Signals a MissingBackpressureException in case the downstream can't keep up.
+     */
+    ERROR,
+    /**
+     * Buffers <em>all</em> onNext values until the downstream consumes it.
+     */
+    BUFFER,
+    /**
+     * Drops the most recent onNext value if the downstream can't keep up.
+     */
+    DROP,
+    /**
+     * Keeps only the latest onNext value, overwriting any previous value if the
+     * downstream can't keep up.
+     */
+    LATEST,
+
+    /**
+     * default strategy,do nothing
+     */
+    DEFAULT
+}
