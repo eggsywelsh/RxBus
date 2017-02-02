5f0a7df04eaf69ce3ce0a91ae88b52d892a954fb
eggsywelsh <eggsywelsh@163.com>
2017-02-02 14:20:28 +0800

    add and modify sample activity

--- sample/src/main/java/com/eggsy/rxbus/sample/MainActivity.java
+++ sample/src/main/java/com/eggsy/rxbus/sample/MainActivity.java
@@ -1,9 +1,16 @@
 package com.eggsy.rxbus.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.eggsy.rxbus.RxBus;
 import com.eggsy.rxbus.annotation.EventSubscribe;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        RxBus.register(this);
     }
 
     @EventSubscribe
-    public void test(String testParam ){
+    public void test(String testParam) {
+        Toast.makeText(this, testParam, Toast.LENGTH_SHORT).show();
+    }
 
+    @OnClick(R.id.btn_test)
+    public void clickStepTest(View v){
+        Intent postEventIntent = new Intent(this,PostEventActivity.class);
+        startActivity(postEventIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RxBus.unRegister(this);
     }
 }
---
+++ sample/src/main/java/com/eggsy/rxbus/sample/PostEventActivity.java
@@ -0,0 +1,30 @@
+package com.eggsy.rxbus.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.eggsy.rxbus.RxBus;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by eggsy on 17-2-2.
+ */
+
+public class PostEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_post)
+    public void clickPostEvent(View view){
+        RxBus.post("eggsy test!!!");
+    }
+}
