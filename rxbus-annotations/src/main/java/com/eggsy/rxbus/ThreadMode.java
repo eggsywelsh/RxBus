e10fa81c29e1539da2267cb3fdac07cf89ebf50b
eggsywelsh <eggsywelsh@163.com>
2017-02-02 20:55:47 +0800

    modify thread mode

--- rxbus-annotations/src/main/java/com/eggsy/rxbus/ThreadMode.java
+++ rxbus-annotations/src/main/java/com/eggsy/rxbus/ThreadMode.java
@@ -22,20 +22,29 @@ public enum ThreadMode {
     MainThread,
 
     /**
-     * Subscriber will be called in a background thread. If posting thread is not the main thread, event handler methods
-     * will be called directly in the posting thread. If the posting thread is the main thread, EventBus uses a single
-     * background thread, that will deliver all its events sequentially. Event handlers using this mode should try to
-     * return quickly to avoid blocking the background thread.
+     *
      */
-    BackgroundThread,
+    IoThread,
 
     /**
-     * Event handler methods are called in a separate thread. This is always independent from the posting thread and the
-     * main thread. Posting events never wait for event handler methods using this mode. Event handler methods should
-     * use this mode if their execution might take some time, e.g. for network access. Avoid triggering a large number
-     * of long running asynchronous handler methods at the same time to limit the number of concurrent threads. EventBus
-     * uses a thread pool to efficiently reuse threads from completed asynchronous event handler notifications.
+     *
      */
-    Async
+    NewThread,
+
+    /**
+     *
+     */
+    Single,
+
+    /**
+     *
+     */
+    Computation,
+
+    /**
+     *
+     */
+    Trampoline
+
 
 }
